29a2eff82e4846948f1916de34134e27e871111b
Imaad747 <imaadtheawesome@gmail.com>
2024-11-27 19:06:47 +1000

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+thedictionares={"Drogging":"Sick; vomiting and crying",
+                "Eversting":"Resting on a bunch of things, or people, or animals",
+                "Guggle":"Holding a glass of something poisonous",
+                "Yohing":"Cackling and getting a deadly weapon",
+                "Patoolum":"Make a terrible spell that fires 360 degrees around the guy who used the spell"}
+print(thedictionares["Yohing"])
+print(thedictionares.get("sloothing"))
+#adding a key value pair
+thedictionares["kazooking"]="Making somebody blast into the air and once they land a banana peel, and cake splats on their face."
+print(thedictionares)
