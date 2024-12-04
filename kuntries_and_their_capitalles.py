fdfc75dac2e08e3b6ec78a73dfa572aea73d3df0
Imaad747 <imaadtheawesome@gmail.com>
2024-12-04 18:07:21 +1000

    Add files via upload

---
+++ kuntries_and_their_capitalles.py
@@ -0,0 +1,22 @@
+kuntries={"USA":"Washington D.C",
+          "UK":"London",
+          "Russia":"Moscow",
+          "China":"Beijing",
+          "Australia":"Canberra",
+          "France":"Paris",
+          "Germany":"Berlin",
+          "Pakistan":"Islamabad",
+          "Egypt":"Cairo",
+          "Saudi Arabia":"Riyadh",
+          "Turkey":"Istanbul",
+          "India":"New Delhi",
+          "Nigeria":"Lagos",
+          "Serbia":"Belgrade",
+          "Italy":"Rome",
+          "Japan":"Tokyo",
+          "Poland":"Warsaw",
+          "Afghanistan":"Kabul",
+          "Netherlands":"Amsterdam",
+          "Vietnam":"Ho Chi Minh City"}
+print(kuntries["Poland"])
+print(kuntries)
