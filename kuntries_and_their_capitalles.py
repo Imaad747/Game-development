5111502f9fc3f4fe1f1cbabfbcc32cd19c421977
Imaad747 <imaadtheawesome@gmail.com>
2024-12-04 19:06:43 +1000

    Add files via upload

--- kuntries_and_their_capitalles.py
+++ kuntries_and_their_capitalles.py
@@ -1,4 +1,4 @@
-kuntries={"USA":"Washington D.C",
+"""kuntries={"USA":"Washington D.C",
           "UK":"London",
           "Russia":"Moscow",
           "China":"Beijing",
@@ -19,4 +19,46 @@ kuntries={"USA":"Washington D.C",
           "Netherlands":"Amsterdam",
           "Vietnam":"Ho Chi Minh City"}
 print(kuntries["Poland"])
+kuntries["Mexico"]="Mexico City"
 print(kuntries)
+print(kuntries.keys())
+print(kuntries.values())
+
+#nested dictionaries
+claasrom={"Bob":{"Grade":82,"Marks":[12,2,60,99,1,3,4]},
+          "Oof":{"Grade":-3,"Marks":[99,99,99,99,99,99,99]}}
+print(claasrom["Oof"]["Marks"])
+
+
+#voooowel finder
+uiy=input("Enter a sentence for the vooowel.")
+vowels={"a":0,
+       "e":0,
+       "i":0,
+       "o":0,
+       "u":0}
+for item in uiy:
+    if item in vowels:
+        vowels[item]+=1
+print(vowels)"""
+
+
+#creating a panogram program
+a={}
+import string
+for i in string.ascii_lowercase:
+    a[i]=0
+print(a)
+stoprudderinput=input("Enter or type in a sentence.")
+for iteem in stoprudderinput:
+    if iteem in a:
+        a[iteem]+=1
+print(a)
+check=True
+for i in a.values():
+    if i==0:
+        check=False
+if check==True:
+    print("It is a panagram.")
+else:
+    print("It is not a panagram")
