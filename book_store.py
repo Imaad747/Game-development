6df7e3ebe63b8a3126247a314be6dca0a87e814a
Imaad747 <imaadtheawesome@gmail.com>
2024-12-11 13:10:19 +0500

    Add files via upload

---
+++ book_store.py
@@ -0,0 +1,7 @@
+booklearstockpile={"Physics Book":"200",
+                   "English Book":"910",
+                   "Maths Book":"892"}
+bk=input("Which book do you want to buy? There is a Physics Book, a English Book, and a Maths Book.")
+print(booklearstockpile[bk])
+print("The book you chose costs:",booklearstockpile[bk])
+print(booklearstockpile)
