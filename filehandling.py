b441848ef27e7a9c0b846d5b82f29cbbae861bac
Imaad747 <imaadtheawesome@gmail.com>
2024-12-18 13:33:08 +0500

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,8 @@
+with open("3D Shapes.txt","w") as f:
+    f.write("Triangular based pyramid, Cuboid, Cube, ")
+#with open("3D Shapes.txt","r") as f:
+ #   print(f.read())
+with open("3D Shapes.txt","a") as f:
+    f.write("Square based pyramid, not a square pyramid, cone, sphere, cylinder")
+with open("3D Shapes.txt","r") as f:
+    print(f.read())
