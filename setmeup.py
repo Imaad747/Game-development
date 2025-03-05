42b18f82740879078ac431e1bd08cabcff214201
Imaad747 <imaadtheawesome@gmail.com>
2025-03-05 19:01:46 +1000

    Add files via upload

---
+++ setmeup.py
@@ -0,0 +1,30 @@
+iwassetforatrap={1,2,3,4,5,6,7,8,9,9,9,9,9,9,9,9,9,9}
+print(iwassetforatrap)
+#print(iwassetforatrap[5]) extinct specimen
+iwassetforatrap.add("I explode traps")
+print(iwassetforatrap)
+Isetamousetrap={2,4,6,8,10,12,12,14,16,18,201}
+
+#union of sets
+
+print(iwassetforatrap|(Isetamousetrap))
+
+print(iwassetforatrap.union(Isetamousetrap))
+
+#intersections
+
+print(iwassetforatrap.intersection(Isetamousetrap))
+
+#you can do it also like this:
+
+print(iwassetforatrap & Isetamousetrap)
+
+#difference of sets
+
+print(iwassetforatrap-Isetamousetrap)
+
+#symetric difference
+
+print(iwassetforatrap^Isetamousetrap)
+
+youArethetrap={233333333333333333333333333333333333333333}
