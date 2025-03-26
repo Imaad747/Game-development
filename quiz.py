6493e4a75b28a99bbcc4ec480ac2f2bae3f22b69
Imaad747 <imaadtheawesome@gmail.com>
2025-03-26 18:55:43 +1000

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -2,6 +2,7 @@ import pgzrun
 TITLE="Quiz master"
 HEIGHT=600
 WIDTH=850
+the_void=False
 thelistofshame=[]
 quitter=0
 totalquitter=0
@@ -14,6 +15,7 @@ option1=Rect(20,320,300,120)
 option2=Rect(20,450,300,120)
 option3=Rect(350,320,300,120)
 option4=Rect(350,450,300,120)
+the_option_archive=[option1,option2,option3,option4]
 skipyoutubead=Rect(660,320,170,250)
 def draw():
     screen.fill("Dark Green")
@@ -29,8 +31,33 @@ def draw():
     screen.draw.textbox(le_message,bigbillboard_ad,color="Gold")
     screen.draw.textbox(str(timebomb),timebox,color="Black",shadow=(0.5,0.5),scolor="Red")
     screen.draw.textbox("Don't Skip",skipyoutubead,color="Black",shadow=(0.5,0.5),scolor="Red",angle=-90)
-
-
+    screen.draw.textbox(les_questions[0],questionbox,color="White")
+    user_id=1
+    for i in the_option_archive:
+       screen.draw.textbox(les_questions[user_id],i,color="White")
+       user_id+=1
+def the_end_dimension():
+   global the_void,the_final_essay,les_questions,timebomb
+   the_void=True
+   the_final_essay=(f"Game over :D. You got {scorepelos}/{totalquitter} ")
+   les_questions=[the_final_essay,"N/A","N/A","N/A","N/A",1992]
+   timebomb=0
+def on_mouse_down(pos):
+   one=1
+   for i in the_option_archive:
+      if i.collidepoint(pos):
+         if one is int(les_questions[5]):
+            namee()
+         else:
+            the_end_dimension()
+      one+=1
+      
+def diffuse():
+   global timebomb
+   if timebomb:
+      timebomb-=1
+   else:
+      the_end_dimension()
 def update():
   bigbillboard_ad.x-=2
   if bigbillboard_ad.right<0:
@@ -42,6 +69,12 @@ def javascribe():
       for q in file:
          thelistofshame.append(q)
          totalquitter+=1
+def evilville_castle_of_the_first_fan_made_minecraft_video_lost_media():
+   global quitter
+   quitter+=1
+   return thelistofshame.pop(0).split("|")
 javascribe()
-print(thelistofshame)
+les_questions=evilville_castle_of_the_first_fan_made_minecraft_video_lost_media()
+print(les_questions)
+clock.schedule_interval(diffuse,1)
 pgzrun.go()
